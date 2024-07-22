b60db85e0315d230613f00c1a589c1dcdab5c3cc
schneibley <Schneibley@gmail.com>
2024-07-22 16:02:57 -0400

    set up routes in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,18 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+//API Route
+const apiRoutes = require('./routes/apiRoutes');
+app.use('/api/notes', apiRoutes);
+//HTML Route
+const htmlRoutes = require('./routes/htmlRoutes');
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+  });
